c817caa5a6aba82af920117cb4f4347778b09457
Barna Adrian <adrianbarna88@gmail.com>
2024-02-26 19:17:55 +0200

    ClassControllerIntegrationTest added

--- src/test/java/com/edusphere/controllers/ClassControllerIntegrationTest.java
+++ src/test/java/com/edusphere/controllers/ClassControllerIntegrationTest.java
@@ -1,28 +1,37 @@
 package com.edusphere.controllers;
 
 import com.edusphere.controllers.utils.TestUtils;
+import com.edusphere.entities.ClassEntity;
 import com.edusphere.entities.OrganizationEntity;
 import com.edusphere.entities.RoleEntity;
 import com.edusphere.entities.UserEntity;
-import com.edusphere.repositories.ChildRepository;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.edusphere.controllers.utils.TestUtils.generateRandomString;
 import static com.edusphere.enums.RolesEnum.*;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @AutoConfigureMockMvc
 @SpringBootTest(properties = "spring.config.name=application-test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestPropertySource(properties = "spring.config.location=classpath:/application-test.properties")
+//TODO add the other tests
 public class ClassControllerIntegrationTest {
     public static final String PASSWORD = "123456";
     public static final String CLASS_ENDPOINT = "/class";
@@ -31,9 +40,6 @@ public class ClassControllerIntegrationTest {
 
     @Autowired
     private TestUtils testUtils;
-
-    @Autowired
-    private ChildRepository childRepository;
 
     private final List<UserEntity> allowedUsersToCallTheEndpoint = new ArrayList<>();
     private final List<UserEntity> notAllowedUsersToCallTheEndpoint = new ArrayList<>();
@@ -51,29 +57,35 @@ public class ClassControllerIntegrationTest {
         notAllowedUsersToCallTheEndpoint.add(testUtils.saveUser(generateRandomString(), PASSWORD, organizationEntity, parentRole));
     }
 
-//    @Test
-//    public void getAllClasses() {
-//        OrganizationEntity organizationEntity = testUtils.saveOrganization();
-//        testUtils.saveAClassInOrganization(organizationEntity);
-//        try {
-//            for (UserEntity allowedUser : allowedUsersToCallTheEndpoint) {
-//                getAllClasses(allowedUser);
-//            }
-//        } catch (Exception e) {
-//            fail("Test failed due to an exception: " + e.getMessage());
-//        }
-//    }
+    @Test
+    public void getAllClasses() {
 
-//    private void getAllClasses(UserEntity userEntity) throws Exception {
-//        String token = testUtils.getTokenForUser(userEntity.getUsername(), PASSWORD);
-//
-//        mockMvc.perform(MockMvcRequestBuilders.get(CLASS_ENDPOINT)
-//                        .header("Authorization", "Bearer " + token)
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$.[*].name", hasItem(childEntity.getName())))
-//                .andExpect(jsonPath("$[*].name", not(hasItem(childFromAnotherOrganization.getName()))));
-//    }
+        try {
+            for (UserEntity allowedUser : allowedUsersToCallTheEndpoint) {
+                getAllClasses(allowedUser);
+            }
+        } catch (Exception e) {
+            fail("Test failed due to an exception: " + e.getMessage());
+        }
+    }
+
+    private void getAllClasses(UserEntity userEntity) throws Exception {
+        ClassEntity aClass = testUtils.saveAClassInOrganization(userEntity.getOrganization());
+        ClassEntity anotherClass = testUtils.saveAClassInOrganization(userEntity.getOrganization());
+
+        OrganizationEntity anptherOrganization = testUtils.saveOrganization();
+        ClassEntity classFromAnotherOrganization = testUtils.saveAClassInOrganization(anptherOrganization);
+
+        String token = testUtils.getTokenForUser(userEntity.getUsername(), PASSWORD);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(CLASS_ENDPOINT)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.[*].id", hasItem(aClass.getId())))
+                .andExpect(jsonPath("$.[*].id", hasItem(anotherClass.getId())))
+                .andExpect(jsonPath("$.[*].id", not(hasItem(classFromAnotherOrganization.getId()))));
+    }
 
 }
