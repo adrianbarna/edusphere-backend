cf7f715b286b126b83e635d7009053fbc490fac9
Barna Adrian <adrianbarna88@gmail.com>
2024-02-26 19:02:04 +0200

    replaces testUtils.saveOrganization(generateRandomString(), generateRandomString()) with test.saveOrganization()

--- src/main/java/com/edusphere/exceptions/RoleNotFoundException.java
+++ src/main/java/com/edusphere/exceptions/RoleNotFoundException.java
@@ -5,4 +5,8 @@ public class RoleNotFoundException extends RuntimeException {
     public RoleNotFoundException(Integer roleId) {
         super("Rolul cu id-ul " + roleId + " este invalid pentru organizatia curenta" );
     }
+
+    public RoleNotFoundException(String message) {
+        super(message);
+    }
 }
